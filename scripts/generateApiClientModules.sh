78e891af6369bc1edce20e58ef79c97c6bce595a
Manda Wilson <1458628+mandawilson@users.noreply.github.com>
2023-04-28 17:06:31 -0400

    try version 6 2 1

--- scripts/generateApiClientModules.sh
+++ scripts/generateApiClientModules.sh
@@ -7,12 +7,12 @@ SWAGGER_CODEGEN_CLI_JAR="$SCRIPT_DIR/swagger-codegen-cli.jar"
 # only download the client if it is not already downloaded
 if [ ! -f ${SWAGGER_CODEGEN_CLI_JAR} ]; then
     if which curl ; then
-        curl 'https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar' --output ${SWAGGER_CODEGEN_CLI_JAR}
+        curl 'https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.2.1/openapi-generator-cli-6.2.1.jar' --output ${SWAGGER_CODEGEN_CLI_JAR}
     else
         if which wget ; then
-            wget 'http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar' -O ${SWAGGER_CODEGEN_CLI_JAR}
+            wget 'https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.2.1/openapi-generator-cli-6.2.1.jar' -O ${SWAGGER_CODEGEN_CLI_JAR}
         else
-            echo "Error : could not find curl or wget for downloading swagger-codegen-cli" 1>&2
+            echo "Error : could not find curl or wget for downloading openapi-generator-cli" 1>&2
             exit 1
         fi
     fi
@@ -23,6 +23,6 @@ rm -rf "${SCRIPT_DIR_PARENT}/genome-nexus-public-api"
 rm -rf "${SCRIPT_DIR_PARENT}/genome-nexus-internal-api"
 
 # generate java modules (see config json files for more details)
-#java -jar ${SWAGGER_CODEGEN_CLI_JAR} config-help -l go
-java -jar ${SWAGGER_CODEGEN_CLI_JAR} generate -l go -c genomeNexusPublicApiClientConfig.json -i ${GENOME_NEXUS_API_DOCS} -o "$SCRIPT_DIR_PARENT/genome-nexus-public-api" --api-package genome_nexus_public_api
-java -jar ${SWAGGER_CODEGEN_CLI_JAR} generate -l go -c genomeNexusInternalApiClientConfig.json -i ${GENOME_NEXUS_API_DOCS}?group=internal -o "$SCRIPT_DIR_PARENT/genome-nexus-internal-api" --api-package genome_nexus_internal_api
+#java -jar ${SWAGGER_CODEGEN_CLI_JAR} config-help -g go
+java -jar ${SWAGGER_CODEGEN_CLI_JAR} generate -g go -i ${GENOME_NEXUS_API_DOCS} -o "$SCRIPT_DIR_PARENT/genome-nexus-public-api" --skip-validate-spec --additional-properties=packageName=genome_nexus_public_api
+java -jar ${SWAGGER_CODEGEN_CLI_JAR} generate -g go -i ${GENOME_NEXUS_API_DOCS} -o "$SCRIPT_DIR_PARENT/genome-nexus-internal-api" --skip-validate-spec --additional-properties=packageName=genome_nexus_internal_api
